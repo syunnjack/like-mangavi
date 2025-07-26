90897ecc024081dc21c3f36e53a6d1eaf4d124b2
syunnjack <syunnjack@gmail.com>
2025-07-26 21:01:17 +0900

    Initial commit like-mangavi

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@include('commons.header')
+        <div class='container bg-primary'>
+            <span class='h1'>ブートストラップのテスト</span>
+        </div>
+@include('commons.footer')
+        
+
---
+++ laravel/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/','App\Http\Controllers\PageController@index');
+
+//Route::get('/','App\Http\Controller,')
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
